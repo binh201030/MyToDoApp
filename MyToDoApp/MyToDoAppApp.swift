e403aefb6a936fd6ef09ff929f58dc30bfa94916
binh201030 <binh201030@gmail.com>
2026-04-23 05:35:22 +1000

    Initial Commit

---
+++ MyToDoApp/MyToDoAppApp.swift
@@ -0,0 +1,32 @@
+//
+//  MyToDoAppApp.swift
+//  MyToDoApp
+//
+//  Created by NTB on 23/4/2026.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct MyToDoAppApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
